eac48e162340a837d9c5af37dd5bce22b59226f1
Eric Mena <53233372+EricM76@users.noreply.github.com>
2021-10-01 17:44:44 -0300

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -17,10 +17,11 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT
   }
 }
